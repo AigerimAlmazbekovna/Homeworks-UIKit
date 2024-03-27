f79e0be3329eb933a3fbcb6473cb2c4085991a47
Aigerim Almazbekova <aigerimalmazbekovna1416@gmail.com>
2024-03-27 13:54:37 +0600

    Initial commit

---
+++ Navigation5/Post.swift
@@ -0,0 +1,16 @@
+//
+//  Post.swift
+//  Navigation5
+//
+//  Created by Айгерим on 27.03.2024.
+//
+
+import Foundation
+import UIKit
+struct Post {
+    let author: String
+    let description: String
+    let image: String
+    let likes: Int
+    let views: Int
+}
