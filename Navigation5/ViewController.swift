a84c9cdcafa7b283f7e6bbf8b9857d3e36708c57
Aigerim Almazbekova <aigerimalmazbekovna1416@gmail.com>
2024-03-27 13:40:53 +0600

    Initial Commit

---
+++ Navigation5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Navigation5
+//
+//  Created by Айгерим on 27.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
